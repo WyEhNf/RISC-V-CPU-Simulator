cb5da1775e217cab92aaa9c3cef629e5072ab7d2
“WyEhNf” <1716096609@qq.com>
2026-08-02 17:01:03 +0800

    fix:a naming mistake in type.h

--- include/sim/types.h
+++ include/sim/types.h
@@ -20,7 +20,7 @@ constexpr u32 MemorySize = 1 << 18, RN = 32, RBN = 32, RSN = 32, LSN = 32,
 enum Op : u8 { R, IA, IS, IM, IC, S, B, U, J, FN, INV };
 enum AF : u8 { ADD, SUB, AND, OR, XOR, SLL, SRL,SRA,SLT, SLTU, NOP };
 enum BF : u8 { EQ, NE, LT, LTU, GE, GEU, NOBR };
-enum MF : u8 { MW, MH, MHU, MV, MBU,SW, SH, SB, NOM };
+enum MF : u8 { MW, MH, MHU, MB, MBU,SW, SH, SB, NOM };
 enum TK : u8 { TN, TROB, TRS, TLSQ };
 
 struct Tag {
