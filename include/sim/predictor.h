7e885a9c5696b17663e748334c035ead3756c96c
“WyEhNf” <1716096609@qq.com>
2026-08-03 17:09:08 +0800

    implement regfile

--- include/sim/predictor.h
+++ include/sim/predictor.h
@@ -1,54 +1,49 @@
-#pragma once 
+#pragma once
 #include "types.h"
 #include <cstdio>
-namespace sim
-{
-struct Predictor
-{
-    i32 t2[BHTN][4];
-    u8 h[BHTN]; i32 l[BHTN];
-    i32 g; 
-    u32 tot,hit;
-    void reset()
-    {
-        for(u32 i=0;i<BHTN;i++)
-        {
-            h[i]=0; 
-            for(int j=0;j<4;++j)
-                t2[i][j]=2;
-            l[i]=2;
-        }   
-        g=2;
-        tot=hit=0;
+namespace sim {
+struct Predictor {
+  i32 t2[BHTN][4];
+  u8 h[BHTN];
+  i32 l[BHTN];
+  i32 g;
+  u32 tot, hit;
+  void reset() {
+    for (u32 i = 0; i < BHTN; i++) {
+      h[i] = 0;
+      for (int j = 0; j < 4; ++j)
+        t2[i][j] = 2;
+      l[i] = 2;
     }
-    u8 predict(u32 pc) const{
-        u16 ix=(u16)(pc&0xFFF);
-        u8 tp=t2[ix][h[ix]] < 3 ? 1 : 0;
-        return (u8)((l[ix] < 3 ? 1 : 0) <<2 |(g<3 ? 1 : 0)<<1 | tp);
-    }
-    void update(u32 pc,bool taken,u8 pred)
-    {
-        tot++;
-        if(taken==(pred&1)) ++hit;
-        u16 ix=(u16)(pc&0xFFF);
-        auto sat=[](i32 &x,bool y)
-        {
-            if(y){
-                if(x>1)--x;
-            }else
-                if(x<4)++x;
-        };
-        sat(l[ix],taken);
-        sat(g,taken);
-        sat(t2[ix][h[ix]],taken);
-        h[ix]=(u8)((h[ix]<<1|taken)&3); 
-    }
-    void report() const
-    {
-        fprintf(stderr, "Predict %u/%u (%.2f%%)\n", hit, tot,
+    g = 2;
+    tot = hit = 0;
+  }
+  u8 predict(u32 pc) const {
+    u16 ix = (u16)(pc & 0xFFF);
+    u8 tp = t2[ix][h[ix]] < 3 ? 1 : 0;
+    return (u8)((l[ix] < 3 ? 1 : 0) << 2 | (g < 3 ? 1 : 0) << 1 | tp);
+  }
+  void update(u32 pc, bool taken, u8 pred) {
+    tot++;
+    if (taken == (pred & 1))
+      ++hit;
+    u16 ix = (u16)(pc & 0xFFF);
+    auto sat = [](i32 &x, bool y) {
+      if (y) {
+        if (x > 1)
+          --x;
+      } else if (x < 4)
+        ++x;
+    };
+    sat(l[ix], taken);
+    sat(g, taken);
+    sat(t2[ix][h[ix]], taken);
+    h[ix] = (u8)((h[ix] << 1 | taken) & 3);
+  }
+  void report() const {
+    fprintf(stderr, "Predict %u/%u (%.2f%%)\n", hit, tot,
             tot ? 100.0 * hit / tot : 0.0);
-    }
-
+  }
 };
 
-}
+} // namespace sim
---
+++ include/sim/regfile.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "sim/types.h"
+namespace sim {
+struct RegFile {
+  u32 rg[RN];
+  Tag rt[RN];
+  void reset() {
+    memset(rg, 0, sizeof rg);
+    rg[0] = 0, rt[0] = Tag{};
+  }
+  void write_reg(u8 rd, u32 val) {
+    if (rd)
+      rg[rd] = val;
+  }
+  void clear_rat(u8 rd, Tag t) {
+    if (rd && rt[rd] == t)
+      rt[rd] = Tag{};
+  }
+  void set_rat(u8 rd, Tag t) {
+    if (rd)
+      rt[rd] = t;
+  }
+  u32 arch(u8 rd) const { return rd ? rg[rd] : 0; }
+  Tag rat_cur(u8 rd) const { return rd ? rt[rd] : Tag{}; }
+};
+
+} // namespace sim
